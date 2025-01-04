cda69992f66a35f12f365e6d4318747cb6acdfd9
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-01-05 03:07:10 +0900

    [queue]Add queue output

--- queue/queue.c
+++ queue/queue.c
@@ -3,7 +3,6 @@
 #include <gst/audio/audio-info.h>
 #include <glib.h>
 
-static int bus_call_count = 0;
 static int on_new_sample_count = 0;
 static GstClockTime total_duration = 0;
 static gsize total_samples = 0;
@@ -11,9 +10,6 @@ static gsize total_samples = 0;
 static gboolean
 bus_call(GstBus *bus, GstMessage *msg, gpointer data)
 {
-  g_print("bus_call: %d\n", bus_call_count++);
-  g_print("message type: %s(%d)\n", gst_message_type_get_name(GST_MESSAGE_TYPE(msg)), GST_MESSAGE_TYPE(msg));
-
   switch (GST_MESSAGE_TYPE(msg)) {
     case GST_MESSAGE_EOS:
       g_print("End of stream\n");
@@ -44,7 +40,6 @@ on_new_sample(GstElement *sink, gpointer data)
 {
   GstSample *sample;
   GstBuffer *buffer;
-  GstBufferList *buffer_list;
   GstMapInfo map_info;
   GstCaps *caps;
   GstStructure *structure;
@@ -65,34 +60,22 @@ on_new_sample(GstElement *sink, gpointer data)
     g_print("caps is NULL\n");
     return GST_FLOW_ERROR;
   }
-  g_print("caps: %s\n", gst_caps_to_string(caps));
-  guint n_structures = gst_caps_get_size(caps);
-  g_print("caps structures should be 1, actually %d\n", n_structures);
   structure = gst_caps_get_structure(caps, 0);
   if (structure == NULL) {
     g_print("structure is NULL\n");
     return GST_FLOW_ERROR;
   }
-  const gchar *name = gst_structure_get_name(structure);
-  g_print("structure name: %s\n", name);
 
   audio_info = gst_audio_info_new_from_caps(caps);
   if (audio_info == NULL) {
     g_print("audio_info is NULL\n");
     return GST_FLOW_ERROR;
   }
-  g_print("channels: %d, flags: %d\n", audio_info->channels, audio_info->flags);
   audio_finfo = audio_info->finfo;
   if (audio_finfo == NULL) {
     g_print("audio_finfo is NULL\n");
     return GST_FLOW_ERROR;
   }
-  g_print("audio format: %s, width: %d, depth: %d, unpack_format: %s, has unpack_func: %s\n",
-          gst_audio_format_to_string(audio_finfo->format),
-          audio_finfo->width,
-          audio_finfo->depth,
-          gst_audio_format_to_string(audio_finfo->unpack_format),
-          audio_finfo->unpack_func ? "true" : "false");
 
   buffer = gst_sample_get_buffer(sample);
   if (buffer == NULL) {
@@ -101,11 +84,9 @@ on_new_sample(GstElement *sink, gpointer data)
   }
   duration = buffer->duration;
   if (duration == GST_CLOCK_TIME_NONE) {
-    g_print("duration is GST_CLOCK_TIME_NONE\n");
   } else {
     duration = duration / audio_info->channels;
     total_duration += duration;
-    g_print("buffer duration: %llums\n", duration / 1000 / 1000);
   }
 
   const gboolean result = gst_buffer_map(buffer, &map_info, GST_MAP_READ);
@@ -118,25 +99,26 @@ on_new_sample(GstElement *sink, gpointer data)
   g_print("%lu samples\n", n_samples);
   total_samples += n_samples;
 
-  buffer_list = gst_sample_get_buffer_list(sample);
-  g_print("buffer_list: %s\n", buffer_list ? "non-NULL" : "NULL");
-
   gst_sample_unref(sample);
 
   return GST_FLOW_OK;
 }
 
 void
-on_eos(GstElement *appsink, gpointer user_data)
+queue_callback(GstElement *queue, gpointer udata)
 {
-  g_print("on_eos\n");
+  gchar *signal;
+
+  signal = (gchar *)udata;
+  g_print("queue_callback: %s\n", signal);
+
 }
 
 int main(int argc, char *argv[])
 {
   GMainLoop *loop;
 
-  GstElement *pipeline, *source, *parse, *convert, *sink;
+  GstElement *pipeline, *source, *parse, *convert, *queue, *sink;
   GstBus *bus;
   GstCaps *caps;
   guint bus_watch_id;
@@ -157,14 +139,23 @@ int main(int argc, char *argv[])
   source = gst_element_factory_make("filesrc", NULL);
   parse = gst_element_factory_make("wavparse", NULL);
   convert = gst_element_factory_make("audioconvert", NULL);
+  queue = gst_element_factory_make("queue", NULL);
   sink = gst_element_factory_make("appsink", NULL);
 
-  if (!pipeline || !source || !sink) {
+  if (!pipeline || !source || !parse || !convert || !queue || !sink) {
     g_printerr("Not all elements could be created.\n");
     return -1;
   }
 
   g_object_set(G_OBJECT(source), "location", filename, NULL);
+
+  g_object_set(G_OBJECT(queue),
+               "min-threshold-buffers", 199,
+               NULL);
+  g_signal_connect(queue, "overrun", G_CALLBACK(queue_callback), "overrun");
+  g_signal_connect(queue, "pushing", G_CALLBACK(queue_callback), "pushing");
+  g_signal_connect(queue, "running", G_CALLBACK(queue_callback), "running");
+  g_signal_connect(queue, "underrun", G_CALLBACK(queue_callback), "underrun");
 
   caps = gst_caps_new_simple("audio/x-raw",
                              "format", G_TYPE_STRING, "S16LE",
@@ -174,9 +165,8 @@ int main(int argc, char *argv[])
                "emit-signals", TRUE,
                NULL);
   g_signal_connect(sink, "new-sample", G_CALLBACK(on_new_sample), NULL);
-  g_signal_connect(sink, "eos", G_CALLBACK(on_eos), NULL);
 
-  gst_bin_add_many(GST_BIN(pipeline), source, parse, convert, sink, NULL);
+  gst_bin_add_many(GST_BIN(pipeline), source, parse, convert, queue, sink, NULL);
   gst_element_link_many(source, parse, convert, sink, NULL);
 
   bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
@@ -191,7 +181,6 @@ int main(int argc, char *argv[])
   g_source_remove(bus_watch_id);
   g_main_loop_unref(loop);
 
-  g_print("bus_call_count: %d\n", bus_call_count);
   g_print("total samples should be %d, actually %lu\n", 45056, total_samples);
   g_print("on_new_sample_count: %d\n", on_new_sample_count);
   g_print("total_duration: %llus\n", total_duration / 1000 / 1000);
